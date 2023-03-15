29010ca409e10440c31d77ecd1f015e5db034623
Filipe Tielson <filipe_thielsom@hotmail.com>
2023-03-15 18:24:28 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -8,14 +8,15 @@ const cors = require("cors")
 const express = require("express")
 const routes = require('./routes')
 
+
 migrationsRun()
 
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
-app.use(cors())
 
 app.use(routes)
 
